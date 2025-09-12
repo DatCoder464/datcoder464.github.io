6f7b332f62e9ec4b74dcf0e94c4a746aa5cfd935
MaxtonGraf <Datcoder.hypothermic@gmail.com>
2025-09-11 22:26:15 -0500

    moved static storage I was using to a seperate folder

---
+++ static/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let button = document.getElementById("button");
+    button.addEventListener("click", () => {
+        button.innerHTML = "<a>Fuck you</a>";
+    })
+})
